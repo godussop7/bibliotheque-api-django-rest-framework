5042d1ceab2c66ede7f79948224b2a9f6a466938
ISEP Student <student@isep.fr>
2026-04-16 23:04:19 +0000

    Add django-jazzmin theme for modern admin interface

--- bibliotheque_project/settings.py
+++ bibliotheque_project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     # Packages tiers
     'rest_framework',      # Django REST Framework
     'django_filters',      # Filtres avancés
+    'jazzmin',             # Thème admin moderne
     # Notre application
     'api',
 ]
@@ -144,6 +145,36 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+# Jazzmin theme configuration
+JAZZMIN_SETTINGS = {
+    'title': 'Bibliothèque API',
+    'header_title': 'Bibliothèque API',
+    'copyright': 'Bibliothèque API',
+    'language_selector': True,
+    'menu': [
+        {
+            'model': 'api.Auteur',
+            'label': 'Auteurs',
+        },
+        {
+            'model': 'api.Livre',
+            'label': 'Livres',
+        },
+        {
+            'model': 'api.Tag',
+            'label': 'Tags',
+        },
+        {
+            'model': 'api.ProfilLecteur',
+            'label': 'Lecteurs',
+        },
+        {
+            'model': 'api.Emprunt',
+            'label': 'Emprunts',
+        },
+    ],
+}
 
 # Configuration REST Framework
 REST_FRAMEWORK = {
