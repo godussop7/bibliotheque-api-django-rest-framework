0b38aaa0dc77886c3112ee89e8e1c9fe31dc4cc0
ISEP Student <student@isep.fr>
2026-04-16 13:53:08 +0000

    Add Railway domain to ALLOWED_HOSTS to fix Bad Request (400) error

--- bibliotheque_project/settings.py
+++ bibliotheque_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-m6h3od&vzo7)i3vj1ol28
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,web-production-f8b47.up.railway.app').split(',')
 
 
 # Application definition
