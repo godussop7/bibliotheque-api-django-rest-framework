614fe6a6c703545f14cf140421548035118a45ed
ISEP Student <student@isep.fr>
2026-04-16 20:46:11 +0000

    Add Whitenoise for serving static files in production to fix admin CSS

---
+++ api/Algo.py
@@ -0,0 +1,4 @@
+N = int(input("Entrez un nombre : "))
+print("Vous avez entré : ",N)
+for i in range(1, N + 1):
+    print(i)
--- bibliotheque_project/settings.py
+++ bibliotheque_project/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -138,8 +139,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Whitenoise configuration
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Configuration REST Framework
 REST_FRAMEWORK = {
